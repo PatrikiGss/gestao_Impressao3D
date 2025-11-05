2383f07f4046144e6ce812b663883865606a3bdf
PatrikiGss <patriki7771@gmail.com>
2025-11-05 01:38:45 -0300

    troca de banco de dados e requirements para instalações futuras em outros pcs

--- Impressora3D/settings.py
+++ Impressora3D/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'Impressora3D.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
